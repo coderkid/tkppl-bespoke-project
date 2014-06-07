788f01f8ed080365a3f04597cb92159e13e808e0
coderkid <wahyoe1st@gmail.com>
2014-06-07 12:31:20 +0700

    add feature scroll up/down and search by keyboard F

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,4 @@
-bespoke.from('article', {
+var deck = bespoke.from('article', {
   keys: true,
   touch: true,
   bullets: '.bullet',
@@ -18,3 +18,36 @@ document.addEventListener('keydown', function(e) {
 		location.href= "#"+jlhSection;
 	}
 });
+
+window.onload = function()
+{
+    //adding the event listerner for Mozilla
+    if(window.addEventListener)
+        document.addEventListener('DOMMouseScroll', moveObject, false);
+ 
+    //for IE/OPERA etc
+    document.onmousewheel = moveObject;
+}
+
+var lastScroll = 0;
+var delta = 0;
+var count = 0;
+function moveObject(event)
+{
+    if (!event) event = window.event;
+    delta = event.wheelDelta;
+    if (delta>lastScroll) {
+      count=0;
+      deck.prev();
+      // alert("atas"+ delta +" " + lastScroll);
+    } else {
+      count++;
+      deck.next();
+      lastScroll = delta;
+      // alert("bawah"+ delta +" " + lastScroll);
+    }
+    if(count>1){
+      lastScroll = 0;
+    }
+
+}
